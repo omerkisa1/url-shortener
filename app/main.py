aab6f9784cdf25c9c11e7aece209d82913ac031f
omerkisa1 <omereis.5555@gmail.com>
2026-04-26 21:47:16 +0300

    add connection configs

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
+from app.config import settings
+from app.database import get_db
 
 app = FastAPI()
 
 @app.get("/health")
 def health_check():
     return {
-        "status" : "ok" 
+        "status" : "ok"
     }
