367a8a8179ad1703772dd17533b41879b7bd42cb
omerkisa1 <omereis.5555@gmail.com>
2026-04-27 09:35:33 +0300

    update link base model

--- app/links/models.py
+++ app/links/models.py
@@ -7,9 +7,9 @@ from app.database import Base
 
 class Link(Base):
     __tablename__ = "links"
-    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, index=True)
-    original_url: Mapped[str]  = mapped_column(String)
-    short_code: Mapped[str]  = mapped_column(String, unique=True, index=True)
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    original_url: Mapped[str]  = mapped_column(String(2048))
+    short_code: Mapped[str]  = mapped_column(String(20), unique=True, index=True)
     click_count: Mapped[int]  = mapped_column(Integer, default=0)
-    created_at: Mapped[datetime] = mapped_column(default=func.now())
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
 
