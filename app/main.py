cc1723d0b66845722cacaddeee035678cc0d9f9e
omerkisa1 <omereis.5555@gmail.com>
2026-04-28 11:23:36 +0300

    add redis configs

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI, Depends
 from app.config import settings
+from contextlib import asynccontextmanager
 from app.database import get_db
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.links.router import router as LinkRouter
+from app.redis import init_redis, close_redis
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_redis()
+    yield 
+    await close_redis()
 
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 app.include_router(LinkRouter, prefix="/link")
 
 @app.get("/health")
