88ee9ddc42e0f0f0e49aef9cf06a18990bcb5655
omerkisa1 <omereis.5555@gmail.com>
2026-04-29 10:35:56 +0300

    add worker service to process click events from RabbitMQ

---
+++ worker.py
@@ -0,0 +1,32 @@
+import asyncio
+import aio_pika
+from sqlalchemy import update
+from app.config import settings
+from app.database import async_session
+from app.links.models import Link
+
+async def process_click(message: aio_pika.IncomingMessage):
+    async with message.process():
+        short_code = message.body.decode()
+        
+        async with async_session() as session:
+            stmt = update(Link).where(Link.short_code == short_code).values(
+                click_count=Link.click_count + 1
+            )
+            await session.execute(stmt)
+            await session.commit()
+            print(f"[WORKER] click_count artırıldı: {short_code}")
+
+async def main():
+    connection = await aio_pika.connect_robust(settings.RABBITMQ_URL)
+    channel = await connection.channel()
+    await channel.set_qos(prefetch_count=10)
+    queue = await channel.declare_queue("click_events", durable=True)
+    
+    print("[WORKER] Queue dinleniyor...")
+    await queue.consume(process_click)
+    
+    await asyncio.Future() 
+
+if __name__ == "__main__":
+    asyncio.run(main())
